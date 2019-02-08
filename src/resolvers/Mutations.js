a98d11f641e919aa7b0e3adc3025c75836a09369
Aleksandr YUrkovskiy <godlikedesigner@gmail.com>
2019-02-08 20:19:33 +0500

    add auth and login logic

--- src/resolvers/Mutations.js
+++ src/resolvers/Mutations.js
@@ -1,4 +1,6 @@
 import hashPassword from '../utils/hashPassword';
+import generateToken from '../utils/generateToken';
+import bcrypt from 'bcryptjs';
 
 const Mutation = {
     async createUser(parent, { data }, { prisma }, info) {
@@ -8,14 +10,43 @@ const Mutation = {
                 ...data,
                 password
             }
+        },info);
+
+        return {
+            token: generateToken(user.id),
+            user
+        }
+    },
+    async login(parent, {data}, { prisma }, info) {
+        const user = await prisma.query.user({
+            where: {
+                email: data.email
+            }
         });
 
-        return user
+        if(!user) {
+            throw new Error('Required user not found!')
+        }
+
+        const isMatch = await bcrypt.compare(data.password, user.password);
+
+        if(!isMatch) {
+            throw new Error('Invalid password!')
+        }
+
+        return {
+            token: generateToken(user.id),
+            user
+        }
     },
     async updateUser(parent, { id, data }, { prisma }, info) {
         const exists = await prisma.exists.User({
             id
         });
+
+        if(typeof data.password === 'string') {
+            data.password = await hashPassword(data.password);
+        }
 
         if(!exists) {
             throw new Error('Required user not found')
@@ -26,7 +57,7 @@ const Mutation = {
                 id
             },
             data
-        })
+        }, info);
     },
     async deleteUser(parent, { id }, { prisma }, info) {
         const exists = await prisma.exists.User({
